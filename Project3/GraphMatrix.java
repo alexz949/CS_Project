2f682f4753909c1811b1e8aa47c9ce9b1286a68b
Alex Zhang <zhanl21@wfu.edu>
2022-11-27 22:04:07 -0500

    Finished topological sort

--- Project3/GraphMatrix.java
+++ Project3/GraphMatrix.java
@@ -1,23 +1,26 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 
 public class GraphMatrix implements Graph{
 
     private int[][] array_2D;
-    private int edgeNum = 0;
-    private boolean[][] check_2D;
-    private int nodeNum = 0;
+    private int edgeNum;
+    private boolean[] check;
+    private int nodeNum;
 
     
 
     @Override
     public void init(int n) {
         // TODO Auto-generated method stub
+        //initialize matrix and visited
         array_2D = new int[n][n];
-        check_2D = new boolean[n][n];
+        check = new boolean[n];
+        Arrays.fill(check, false);
         nodeNum = n;
-        
+        edgeNum = 0;
         return;
         
     }
@@ -25,13 +28,14 @@ public class GraphMatrix implements Graph{
     @Override
     public int nodeCount() {
         // TODO Auto-generated method stub
-        
-        return array_2D.length;
+        //return the node number
+        return nodeNum;
     }
 
     @Override
     public int edgeCount() {
         // TODO Auto-generated method stub
+        //return the total edge number
         return edgeNum;
         
     }
@@ -39,8 +43,8 @@ public class GraphMatrix implements Graph{
     @Override
     public void addEdge(int v, int w, int wgt) {
         // TODO Auto-generated method stub
-        
 
+        //adding an edge with [v][w]
         array_2D[v][w] = wgt;
         edgeNum++;
         return;
@@ -51,13 +55,14 @@ public class GraphMatrix implements Graph{
     @Override
     public int getWeight(int v, int w) {
         // TODO Auto-generated method stub
-        
+        //return the weight number
         return array_2D[v][w];
     }
 
     @Override
     public void setWeight(int v, int w, int wgt) {
         // TODO Auto-generated method stub
+        //set the weight number
         array_2D[v][w] = wgt;
         return;
         
@@ -66,6 +71,7 @@ public class GraphMatrix implements Graph{
     @Override
     public void removeEdge(int v, int w) {
         // TODO Auto-generated method stub
+        //set the [v][w] entry to be 0
         array_2D[v][w] = 0;
         edgeNum--;
         return;
@@ -75,6 +81,7 @@ public class GraphMatrix implements Graph{
     @Override
     public boolean hasEdge(int v, int w) {
         // TODO Auto-generated method stub
+        //check the whether has an edge
         if(array_2D[v][w] == 0)
             return false;
         else{
@@ -85,10 +92,11 @@ public class GraphMatrix implements Graph{
     @Override
     public ArrayList<Integer> neighbors(int v) {
         // TODO Auto-generated method stub
+        //looping through the matrix to check whether there is an edge
         ArrayList<Integer> array = new ArrayList<>();
         for (int i = 0; i < array_2D.length; i++) {
-            if(array_2D[v][i] != 0){
-                array.add(array_2D[v][i]);
+            if(array_2D[v][i] != 0){//yes, add into array
+                array.add(i);
             }
             
         }
@@ -98,46 +106,37 @@ public class GraphMatrix implements Graph{
     @Override
     public void resetVisited() {
         // TODO Auto-generated method stub
-        for (int i = 0; i < check_2D.length; i++) {
-            for (int j = 0; j < check_2D[0].length; j++) {
-                check_2D[i][j] = false;
-            }
-        }
+        //set all element to be false
+        Arrays.fill(check, false);
         return;
-
-        
     }
 
     @Override
     public ArrayList<Integer> BFS(int v) {
         // TODO Auto-generated method stub
+        //initialization
         ArrayList<Integer> array = new ArrayList<>();
-
-
         LinkedList<Integer> queue = new LinkedList<>();
+
+
+        //add the starting vertex
         queue.add(v);
-        check_2D[v][v] = true;
+        check[v] = true;
+
+
+        int num1 = 0;
         while(!queue.isEmpty()){
-            int num1 = v;
-            int num2 = 0;
+            num1 = queue.get(0);
+
+            //pop into array
             array.add(queue.remove());
-            while(hasEdge(num1, num2)){
-                if(!check_2D[num1][num2]){
-                    check_2D[num1][num2] = true;
-                    queue.add(num2);
-                    num1 = num2;
-                    num2 = 0;
+            for (int i = 0; i < array_2D.length; i++) {//keep finding neighbor
+                if(array_2D[num1][i] != 0 && check[i] == false){//yes, then push into queue and set as visited.
+                    queue.add(i);
+                    check[i] = true;
                 }
-
-                num2++;
-                if(num2 >= nodeNum)
-                    break;
-
             }
         }
-        
-
-
 
         return array;
     }
@@ -145,8 +144,9 @@ public class GraphMatrix implements Graph{
     @Override
     public boolean hasPath(int v, int w) {
         // TODO Auto-generated method stub
+        //Use bfs to store all possible connecting vertexes
         ArrayList<Integer> array = BFS(v);
-        if(array.contains(w)){
+        if(array.contains(w)){// if vertex w exists, return true
             resetVisited();
             return true;
         }
@@ -155,15 +155,52 @@ public class GraphMatrix implements Graph{
             return false;
         }
 
-
-
-
     }
 
     @Override
     public ArrayList<Integer> topologicalSort() {
         // TODO Auto-generated method stub
-        return null;
+        //initialization
+        ArrayList<Integer> topo = new ArrayList<>();
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] indegree = new int[nodeNum];
+
+        //looping to find the number in coming directions
+        for (int i = 0; i < array_2D.length; i++) {
+            for (int j = 0; j < array_2D.length; j++) {
+                if(array_2D[j][i] != 0){
+                    indegree[i]++;
+                }
+            }
+        }
+
+
+        //find all vertexes with no incoming directions and add them into queue
+        for (int i = 0; i < indegree.length; i++) {
+            if(indegree[i] == 0){
+                queue.add(i);
+                check[i] = true;
+            }
+        }
+
+        //BFS which start from vertexes which have no incoming
+        int num1 = 0;
+        while(!queue.isEmpty()){
+            num1 = queue.get(0);
+
+            topo.add(queue.remove());
+            for (int i = 0; i < array_2D.length; i++) {
+                if(array_2D[num1][i] != 0 && check[i] == false){
+                    queue.add(i);
+                    check[i] = true;
+                }
+            }
+        }
+
+        //return the array
+        return topo;
+
+
     }
-    //TODO.
+
 }
--- Project3/TestGraphMatrix.java
+++ Project3/TestGraphMatrix.java
@@ -8,24 +8,33 @@ public class TestGraphMatrix{
         // For testing GraphMatrix implementation.
 
         GraphMatrix graph = new GraphMatrix();
-        graph.init(4);
+        graph.init(5);
         
-        
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                graph.addEdge(1, j, j+1);
-            }
+        graph.addEdge(0,1,1);
+        graph.addEdge(0,2,1);
+        graph.addEdge(1,4,1);
+        graph.addEdge(1,3,1);
+        graph.addEdge(2,3,1);
+        graph.addEdge(3,4,1);
 
-        }
-        graph.removeEdge(0, 0);
-        graph.addEdge(0, 0, 10);
-        graph.setWeight(0,0,100);
-        System.out.println(graph.hasEdge(0, 1));
+        GraphMatrix graph2 = new GraphMatrix();
+        graph2.init(6);
+        graph2.addEdge(5,2,1);
+        graph2.addEdge(5,0,1);
+        graph2.addEdge(4,1,1);
+        graph2.addEdge(4,0,1);
+        graph2.addEdge(2,3,1);
+        graph2.addEdge(3,1,1);
+
+
+
+        System.out.println("Check whether has path: " + graph.hasEdge(0, 1));
         System.out.println(graph.getWeight(0,0));
-        ArrayList<Integer> neib = new ArrayList<>();
+
+        ArrayList<Integer> neib;
         neib = graph.neighbors(0);
 
-        System.out.println("The neibor should be:");
+        System.out.println("The neighbor should be:");
         for (int i = 0; i < neib.size(); i++) {
             System.out.println(neib.get(i));
         }
@@ -40,7 +49,14 @@ public class TestGraphMatrix{
         graph.resetVisited();
 
 
-        System.out.println(graph.hasPath(0,3));
+        System.out.println("Whether has a path: " + graph.hasPath(0,3));
+
+
+        System.out.println("This is check the topological sort");
+        ArrayList<Integer> sort = graph2.topologicalSort();
+        for (int i = 0; i < sort.size(); i++) {
+            System.out.println(sort.get(i));
+        }
 
 
 
