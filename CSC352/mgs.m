3038d45c31120ac5effa14b09d622fd16a73fe4d
Alex Zhang <zhanl21@wfu.edu>
2023-02-06 19:17:21 -0500

    create matlab q3

---
+++ CSC352/mgs.m
@@ -0,0 +1,14 @@
+function [Q,R] = mgs(A)
+    [n,m] = size(A);
+    Q = zeros(n,m);
+    R = zeros(m,m);
+    for j = 1:m
+        x = A(:,1);
+        for i = 1:j-1
+            R(i,j) = Q(:,i)'* x;
+            x = x - R(i,j) * Q(:,i);
+        end
+        R(j,j) = norm(x)';
+        Q(:,j) = x / R(j,j);
+    end        
+end
