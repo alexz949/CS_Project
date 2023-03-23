965bf58f90e09cf9b0f89a39375ef8c86706dfa4
alexz949 <zhanl21@wfu.edu>
2023-03-23 14:40:33 -0400

    change java

--- CSC301/HW7/knapsack.java
+++ CSC301/HW7/knapsack.java
@@ -3,38 +3,48 @@ import java.util.*;
 import java.io.*;
 
 public class knapsack {
-    static ArrayList<Integer> weights = new ArrayList<>();
-    static ArrayList<Integer> values = new ArrayList<>();
-    static int W;
-    static int n;
+   
     public static void main(String[] args) throws IOException {
-        readFile("CSC301\\HW7\\large.txt");
+        //unit test
+        fileReader readerS = new fileReader("CSC301\\HW7\\small.txt");
+        fileReader readerM = new fileReader("CSC301\\HW7\\medium.txt");
+        fileReader readerL = new fileReader("CSC301\\HW7\\large.txt");
 
-
-        
-        int profit[] = new int[values.size()];
-        int weight[] = new int[weights.size()];
-        for (int i = 0; i < weight.length; i++) {
-            profit[i] = values.get(i);
-            weight[i] = weights.get(i);
+        //reading and printing
+        int profitS[] = new int[readerS.getValue().size()];
+        int weightS[] = new int[readerS.getWeight().size()];
+        for (int i = 0; i < weightS.length; i++) {
+            profitS[i] = readerS.getValue().get(i);
+            weightS[i] = readerS.getWeight().get(i);
         }
-    
-        int ans = knapsack_mem(W, weight, profit,n);
-        System.out.println(ans);
+        System.out.println(knapsack_mem(readerS.W, weightS, profitS,readerS.n));
+
+        int profitM[] = new int[readerM.getValue().size()];
+        int weightM[] = new int[readerM.getWeight().size()];
+        for (int i = 0; i < weightM.length; i++) {
+            profitM[i] = readerM.getValue().get(i);
+            weightM[i] = readerM.getWeight().get(i);
+        }
+        System.out.println(knapsack_mem(readerM.W, weightM, profitM,readerM.n));
+
+
+        int profitL[] = new int[readerL.getValue().size()];
+        int weightL[] = new int[readerL.getWeight().size()];
+        for (int i = 0; i < weightL.length; i++) {
+            profitL[i] = readerL.getValue().get(i);
+            weightL[i] = readerL.getWeight().get(i);
+        }
+        System.out.println(knapsack_mem(readerL.W, weightL, profitL,readerL.n));
         
     }
+
     public static int knapsack_mem(int W, int[] w, int[] val, int n){
+        //2-D array version 
         int bag[][] = new int[n+1][W+1];
-        for(int i = 0; i <= n; i++){
-            bag[i][0] = 0;
-        }
-        for(int i = 0; i <= W; i++){
-            bag[0][i] = 0;
-        }
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= W; j++){
                 if(w[i-1] <= j){
-                    bag[i][j] = Math.max(bag[i-1][j], bag[i-1][j-w[i-1]] + val[i-1]);
+                    bag[i][j] = Math.max(bag[i-1][j], bag[i-1][j-w[i-1]] + val[i-1]); //whether add or not
                 }
                 else{
                     bag[i][j] = bag[i-1][j];
@@ -42,25 +52,6 @@ public class knapsack {
             }
         }
         return bag[n][W];
-    }
-    public static void readFile(String path) throws IOException{
-        File file = new File(path);
-        Scanner sc = new Scanner(file);
-        String info = sc.nextLine();
-        String[] ans = info.split(" ");
-        W = Integer.parseInt(ans[1]);
-        String info2 = sc.nextLine();
-        String[] ans2 = info2.split(" ");
-        n = Integer.parseInt(ans2[1]);
-
-        while(sc.hasNextLine()){
-            String str = sc.nextLine();
-            String[] store = str.split(" ");
-            weights.add(Integer.parseInt(store[1]));
-            values.add(Integer.parseInt(store[3]));
-        }
-        sc.close();
-
     }
     
 }
--- CSC301/HW7/knapsackMem.java
+++ CSC301/HW7/knapsackMem.java
@@ -5,42 +5,61 @@ import java.io.*;
 import java.util.Scanner;
 
 class knapsackMem {
-    static ArrayList<Integer> weights = new ArrayList<>();
-    static ArrayList<Integer> values = new ArrayList<>();
-    static int W;
-    static int n;
     public static void main(String[] args) throws IOException {
-        readFile("CSC301\\HW7\\large.txt");
+        //unit test
+        fileReader readerS = new fileReader("CSC301\\HW7\\small.txt");
+        fileReader readerM = new fileReader("CSC301\\HW7\\medium.txt");
+        fileReader readerL = new fileReader("CSC301\\HW7\\large.txt");
 
-
-        
-        int profit[] = new int[values.size()];
-        int weight[] = new int[weights.size()];
-        for (int i = 0; i < weight.length; i++) {
-            profit[i] = values.get(i);
-            weight[i] = weights.get(i);
+        //reading and printing
+        int profitS[] = new int[readerS.getValue().size()];
+        int weightS[] = new int[readerS.getWeight().size()];
+        for (int i = 0; i < weightS.length; i++) {
+            profitS[i] = readerS.getValue().get(i);
+            weightS[i] = readerS.getWeight().get(i);
         }
-    
-        int ans = knapsack(W, weight, profit,n);
-        System.out.println(ans);
-        
-    }
-    public static int knapsack(int W, int[] w, int[] val, int n){
-        int bag[] = new int[W+1];
-        
-        
+        System.out.println(knapsack(readerS.W, weightS, profitS,readerS.n));
 
+        int profitM[] = new int[readerM.getValue().size()];
+        int weightM[] = new int[readerM.getWeight().size()];
+        for (int i = 0; i < weightM.length; i++) {
+            profitM[i] = readerM.getValue().get(i);
+            weightM[i] = readerM.getWeight().get(i);
+        }
+        System.out.println(knapsack(readerM.W, weightM, profitM,readerM.n));
+
+
+        int profitL[] = new int[readerL.getValue().size()];
+        int weightL[] = new int[readerL.getWeight().size()];
+        for (int i = 0; i < weightL.length; i++) {
+            profitL[i] = readerL.getValue().get(i);
+            weightL[i] = readerL.getWeight().get(i);
+        }
+        System.out.println(knapsack(readerL.W, weightL, profitL,readerL.n));
+    }
+
+    public static int knapsack(int W, int[] w, int[] val, int n){
+        //memory-efficient
+        int bag[] = new int[W+1];
         for(int i = 1; i <= n; i++){
-            for(int j = W; j >= 0; j--){
+            for(int j = W; j >= 0; j--){//each loop leave the weight for that capacity
                 if(w[i-1] <= j){
-                    bag[j] = Math.max(bag[j], bag[j-w[i-1]] + val[i-1]);
+                    bag[j] = Math.max(bag[j-w[i-1]] + val[i-1], bag[j]);
                 }
-              
             }
         }
         return bag[W];
     }
-    public static void readFile(String path) throws IOException{
+    
+    }
+    
+
+class fileReader{
+    ArrayList<Integer> weights = new ArrayList<>();
+    ArrayList<Integer> values = new ArrayList<>();
+    int W;
+    int n;
+    public fileReader(String path) throws IOException{
         File file = new File(path);
         Scanner sc = new Scanner(file);
         String info = sc.nextLine();
@@ -58,6 +77,13 @@ class knapsackMem {
         }
         sc.close();
     }
-    
+    public ArrayList<Integer> getWeight(){
+        return weights;
+    }
+    public ArrayList<Integer> getValue(){
+        return values;
+    }
+
+
 }
 
